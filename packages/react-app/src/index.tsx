ee9a99b7c598c1c3adcdedb2a74d929a505a803a
Kevin von Flotow <vonflow@gmail.com>
2022-01-21 17:01:58 -0700

    move https redirect to html file

--- packages/react-app/src/index.tsx
+++ packages/react-app/src/index.tsx
@@ -4,20 +4,14 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-if (window.location.hostname !== 'localhost' && !window.location.protocol.startsWith('https')) {
-  const url = new URL(window.location.href)
-  url.protocol = 'https:'
-  window.location.assign(url.href)
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-  )
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
 
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals()
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
