b85463fa6aa7f36f9cf4a8e71f427219da1eba99
Deciphersoul <Deciphersoul@gmail.com>
2022-04-05 00:25:34 -0500

    Avax Support

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -70,10 +70,10 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://localhost:8545",
-      /*      
+      /*
         notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
         (you can put in a mnemonic here to set the deployer locally)
-      
+
       */
     },
 
@@ -110,7 +110,13 @@ module.exports = {
         mnemonic: mnemonic(),
       },
     },
+    avax: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
 
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
     rinkeby: {
       url: "https://rinkeby.infura.io/v3/cfeb072b8469447e889da944481d5874", // <---- YOUR INFURA ID! (or it won't work)
 
--- packages/react-app/src/util/getNetworkDataByChainId.ts
+++ packages/react-app/src/util/getNetworkDataByChainId.ts
@@ -221,6 +221,31 @@ export const networks: Record<number, NetworkData> = {
       },
     ],
   },
+  43114: {
+    chainId: 43114,
+    name: 'Avalanche',
+    shortName: 'AVAX',
+    urlName: 'avax',
+    nativeCurrency: {
+      address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+      name: 'Avalanche',
+      symbol: 'AVAX',
+      decimals: 18,
+      balance: BigNumber.from(0),
+    },
+    explorerURL: 'https://snowtrace.io/',
+    rpcURL: 'https://api.avax.network/ext/bc/C/rpc',
+    icon: '/avax.png',
+    isTestNet: false,
+    supportedLiquidityPairTokens: [
+      //
+      {
+        address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+        // pair w/ USDC.e on https://traderjoexyz.com/home#/
+        stablePair: '0xA389f9430876455C36478DeEa9769B7Ca4E3DDB1',
+      },
+    ],
+  },
 }
 
 export default function getNetworkDataByChainId(chainId: number): NetworkData | undefined {
