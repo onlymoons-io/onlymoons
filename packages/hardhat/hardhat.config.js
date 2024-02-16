49182719670bd1c93870b68797e75876533c2ddb
Deciphersoul <Deciphersoul@gmail.com>
2024-02-15 18:54:23 -0600

    merlin

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -187,7 +187,13 @@ module.exports = {
         mnemonic: mnemonic(),
       },
     },
-
+    merlin: {
+			url: "https://rpc.merlinchain.io",
+			// gasPrice: 1000000000,
+			accounts: {
+				mnemonic: mnemonic(),
+			},
+		},
     base: {
       url: "https://developer-access-mainnet.base.org/",
       // gasPrice: 1000000000,
